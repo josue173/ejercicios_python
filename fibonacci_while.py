e785308495f4c9869120eb9bad0224e73eb5e1f5
josue173 <josue.perez04@outlook.com>
2025-08-10 12:30:29 -0600

    feat: comentarios de ayuda

--- fibonacci_while.py
+++ fibonacci_while.py
@@ -5,13 +5,12 @@ hasta = int(input('Nivel fibonacci: '))
 while nivel <= hasta:
   if nivel <= 2:
       inicio = nivel - 1
-      print(inicio)
-      valor_actual = inicio
-      valor_anterior = valor_actual - 1
+      print(inicio)                     # Cuando nivel = 2
+      valor_actual = inicio             # Actual = 1
+      valor_anterior = valor_actual - 1 # Anterior = 0
   else:
-    resultado = valor_actual + valor_anterior
+    resultado = valor_actual + valor_anterior # Al inicio es 1 + 0
     print(resultado)
     valor_anterior = valor_actual
     valor_actual = resultado
   nivel += 1
-  
